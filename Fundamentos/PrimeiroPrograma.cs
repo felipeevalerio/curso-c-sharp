cc626d3ad90554ff69af42783a1885167a70a577
dti Digital <felipeevalerio@outlook.com>
2021-11-08 13:23:21 -0300

    Variaveis e Constantes

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CursoCSharp.Fundamentos
+{
+  class PrimeiroPrograma
+  {
+    public static void Executar()
+    {
+      Console.Write("Teste");
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Variáveis e Constantes - Fundamnetos", VariaveisEConstantes.Executar}
+            });
+
+      central.SelecionarEExecutar();
+    }
+  }
+}
